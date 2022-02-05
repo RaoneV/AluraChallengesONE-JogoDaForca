c144af37b1b88362067bd2b235e467b9c128ad88
Raone Victor da Cunha <raone_victor@hotmail.com>
2022-02-04 22:34:08 -0300

    Verificando se a tecla precionada é uma letra no formato válido

--- js/gerencia-jogo.js
+++ js/gerencia-jogo.js
@@ -1,11 +1,26 @@
 var palavras = ["ALURA", "ORACLE", "CHALLENGE", "PROGRAMACAO", "LOGICA"];
 var botaoIniciar = document.querySelector(".iniciar-jogo");
+var pagina = document.querySelector("body");
 
 botaoIniciar.addEventListener("click", function(){
     criarTabuleiroDaForca();
     
     sortearPalavra();
+
+    letraPrecionada();
 });
+
+function letraPrecionada(){
+    pagina.addEventListener("keypress", function(event){
+        console.log(event.key);
+        if(event.key.replace(/[^A-Z]/g, "") == event.key){
+            console.log("valido");
+        }
+        else{
+            console.log("invalido");
+        }
+    });
+}
 
 function sortearPalavra(){
     sorteado = Math.floor(Math.random() * palavras.length);
